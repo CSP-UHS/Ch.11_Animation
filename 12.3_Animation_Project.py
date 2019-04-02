02e2031bcc3be13b768b0ff361ab0c6da39ad868
Alex <randallale@urbandaleschools.com>
2019-04-02 18:01:43 -0500

    Added Rectangle

--- 12.3_Animation_Project.py
+++ 12.3_Animation_Project.py
@@ -10,15 +10,39 @@ screen_height = 600
 screen_width = 600
 
 
+class Rectangle:
+	def __init__(self, pos_x, pos_y, width, height, color, rotate):
+		self.pos_x = pos_x
+		self.pos_y = pos_y
+		self.width = width
+		self.height = height
+		self.color = color
+		self.rotate = rotate
+
+	def draw_rectangle(self):
+		arcade.draw_rectangle_filled(self.pos_x, self.pos_y, self.width, self.height, self.color, self.rotate)
+
+	def update_rectangle(self):
+		# Logic goes here
+		pass
+
+
 class AnimationProject(arcade.Window):
 	def __init__(self, width, height, title):
 		super().__init__(width, height, title)
+		self.rectangle_list = []
+		for i in range(10):
+			self.rectangle = Rectangle(screen_width/2, screen_height-15, screen_width, 30, arcade.color.PURPLE)
+			self.rectangle_list.append(self.rectangle)
 
 	def on_draw(self):
-		pass
+		for self.rectangle in self.rectangle_list:
+			self.rectangle.draw_rectangle()
 
 	def update(self, dt):
 		pass
+		for self.rectangle in self.rectangle_list:
+			self.rectangle.update_rectangle()
 
 
 def main():
