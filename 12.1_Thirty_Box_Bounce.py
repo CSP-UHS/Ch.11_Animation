0e986ebfc57351006a2ea2a2ecd82a3968fb3249
randallale <randallale@urbandaleschools.com>
2019-04-02 12:16:15 -0500

    Created rectangles

--- 12.1_Thirty_Box_Bounce.py
+++ 12.1_Thirty_Box_Bounce.py
@@ -22,6 +22,24 @@ SW = 600
 SH = 600
 
 
+class Rectangle:
+    def __init__(self, pos_x, pos_y, width, height, color):
+        self.width = width
+        self.height = height
+        self.color = color
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def draw_rectangle(self):
+        arcade.draw_rectangle_filled(self.pos_x, self.pos_y, self.width, self.height, self.color)
+
+    def hit(self):
+        pass
+
+    def update_rectangle(self):
+        pass
+
+
 class Box:
     def __init__(self, pos_x, pos_y, dx, dy, width, height, color):
         self.pos_x = pos_x
@@ -50,18 +68,34 @@ class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.ASH_GREY)
-        self.box = Box(320, 240, 5, 5, 50, 50, arcade.color.AUBURN)
+        self.boxlist=[]
+        self.rectanglelist = []
+        self.rectanglelist.append(Rectangle(15, 300, 30, 600, arcade.color.RED))
+        self.rectanglelist.append(Rectangle(300, 15, 600, 30, arcade.color.RED))
+        self.rectanglelist.append(Rectangle(585, 300, 30, 600, arcade.color.RED))
+        self.rectanglelist.append(Rectangle(300, 585, 600, 30, arcade.color.RED))
+
+        for i in range(30):
+            size = random.randrange(10,50)
+            self.box = Box(random.randrange(50,550), random.randrange(50,550), 5, 5, size, size, arcade.color.AUBURN)
+            self.boxlist.append(self.box)
 
     def on_draw(self):
         arcade.start_render()
-        self.box.draw_box()
-    def on_update(self, dt):
+        for self.box in self.boxlist:
+            self.box.draw_box()
+        for self.rectangle in self.rectanglelist:
+            self.rectangle.draw_rectangle()
 
-        self.box.update_box()
+    def on_update(self, dt):
+        for self.box in self.boxlist:
+            self.box.update_box()
+
 
 
 def main():
-    window = MyGame(SW, SH, "Drawing Example")
+    window = MyGame(SW, SH, "Thirty Box Bounce")
     arcade.run()
+
 
 main()
