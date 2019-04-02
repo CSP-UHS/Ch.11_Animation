14d6dacf70528312dfadf135fec8b9111d4e320f
Alex <randallale@urbandaleschools.com>
2019-04-02 17:26:47 -0500

    Basics

---
+++ 12.3_Animation_Project.py
@@ -0,0 +1,27 @@
+'''
+ANIMATION PROJECT
+-----------------
+Your choice!!! Have fun and be creative.
+
+'''
+import arcade
+
+screen_height = 600
+screen_width = 600
+
+
+class AnimationProject(arcade.Window):
+	def __init__(self, width, height, title):
+		super().__init__(width, height, title)
+
+	def on_draw(self):
+		pass
+
+	def update(self, dt):
+		pass
+
+
+def main():
+	AnimationProject(screen_width, screen_height, "12.3 Animation Project")
+	arcade.run()
+main()
