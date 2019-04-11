2dc503db102102b1163363801acad828efa2eccf
Alex <randallale@urbandaleschools.com>
2019-04-10 20:12:46 -0500

    Finished

--- Clock.py
+++ Clock.py
@@ -11,7 +11,7 @@ CENTER_Y = SCREEN_HEIGHT // 2
 SWEEP_LENGTH = 250
 
 
-def on_draw(delta_time):
+def on_draw(dt):
     x_minute = SWEEP_LENGTH * math.sin(on_draw.minute) + CENTER_X
     y_minute = SWEEP_LENGTH * math.cos(on_draw.minute) + CENTER_Y
 
@@ -26,9 +26,9 @@ def on_draw(delta_time):
     arcade.draw_circle_filled(CENTER_X, CENTER_Y, SWEEP_LENGTH, arcade.color.WHITE)
     # Creates time things
     arcade.draw_text("XII", CENTER_X - 25, CENTER_Y + 200, arcade.color.BLACK, 36)  # 12
-    arcade.draw_text("VI", CENTER_X - 15, CENTER_Y - 235, arcade.color.BLACK, 36)  # 6
-    arcade.draw_text("IX", CENTER_X - 235, CENTER_Y - 20, arcade.color.BLACK, 36)  # 9
-    arcade.draw_text("III", CENTER_X + 200, CENTER_Y - 20, arcade.color.BLACK, 36)  # 3
+    arcade.draw_text("VI", CENTER_X - 22, CENTER_Y - 235, arcade.color.BLACK, 36)  # 6
+    arcade.draw_text("IX", CENTER_X - 235, CENTER_Y - 15, arcade.color.BLACK, 36)  # 9
+    arcade.draw_text("III", CENTER_X + 200, CENTER_Y - 15, arcade.color.BLACK, 36)  # 3
     # Creates clocks hands
     arcade.draw_line(CENTER_X, CENTER_Y, x_minute, y_minute, arcade.color.RED, 12)
     arcade.draw_line(CENTER_X, CENTER_Y, x_hour, y_hour, arcade.color.BLUE, 10)
@@ -38,8 +38,8 @@ def on_draw(delta_time):
                                arcade.color.BROWN_NOSE, 10)
     arcade.draw_circle_filled(CENTER_X, CENTER_Y, 10, arcade.color.BLACK)
     # TEST CLOCK
-    clock = str((time.strftime("%I"), time.strftime("%M"), time.strftime("%S")))
-    arcade.draw_text(clock, CENTER_X-30, SCREEN_HEIGHT-25, arcade.color.PURPLE)
+    clock = str((time.strftime("%I") + time.strftime(":%M:") + time.strftime("%S")))
+    arcade.draw_text(clock, CENTER_X-50, SCREEN_HEIGHT-25, arcade.color.PURPLE)
     #  Basic logic for the clock
     on_draw.second = (6.282/60) * float(time.strftime("%S"))
     on_draw.hour = (6.282 / 12) * float(time.strftime("%I")) + on_draw.minute / 10
@@ -49,7 +49,7 @@ def on_draw(delta_time):
 # Calculates where the hands should be based on the time at the beginning
 on_draw.second = (6.282/60) * float(time.strftime("%S"))
 on_draw.minute = (6.282/60) * float(time.strftime("%M")) + on_draw.second / 60
-on_draw.hour = (6.282/12) * float(time.strftime("%I")) + on_draw.minute/10
+on_draw.hour = (6.282/12) * float(time.strftime("%I")) + on_draw.minute / 10
 
 
 def main():
