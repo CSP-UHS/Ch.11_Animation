b2082b437f83956d23a71819c2160d4eb1b00089
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-02-13 13:43:01 -0600

    Name commit ;P

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -4,9 +4,10 @@ Sign your name: Nellie Leaverton
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+    self.ball = Ball(320, 240, 3, 2, 15, arcade.color.AUBURN)
 2.) What are the variables dx and dy?
-
+    dx = change direction by x
+    dy = change direction by y
 3.) How many pixels/sec does the ball move in the x-direction?
 
 4.) How many pixels/sec does the ball move in the y-direction?
@@ -14,11 +15,11 @@ All questions are about the final code in Chapter 12:
 5.) Which method is run 60 times/second?
 
 6.) What does this code do?   self.dx *= -1
-
+    change the x direction
 7.) What does this code do?  self.pos_y += self.dy
-
+    the y position is now set to the y direction
 8.) What is the width of the window?
-
+    640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
 
 10.) What is this code checking? if self.pos_x < self.rad
