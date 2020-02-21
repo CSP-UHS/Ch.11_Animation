6634268b236f04ad4182d312c5c28ddf9c49bf48
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-02-21 14:14:41 -0600

    Name commit ;P

--- 12.1_Thirty_Box_Bounce.py
+++ 12.1_Thirty_Box_Bounce.py
@@ -5,9 +5,9 @@ You will want to incorporate lists to modify the
 Ball Bounce Program to create the following:
 
     1.) Screen size 600 x 600
-2.) Draw four 30px wide side rails on all four sides of the window
-3.) Make each side rail a different color.
-4.) Draw 30 black boxes(squares) of random size from 10-50 pixels
+    2.) Draw four 30px wide side rails on all four sides of the window
+    3.) Make each side rail a different color.
+    4.) Draw 30 black boxes(squares) of random size from 10-50 pixels
 5.) Animate them starting at random speeds from -300 to +300 pixels/second. 
 6.) All boxes must be moving.
 7.) Start all boxes in random positions between the rails.
@@ -53,15 +53,19 @@ class Ball:
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.ASH_GREY)
+        arcade.set_background_color(arcade.color.WHITE)
         self.ball = Ball(320, 240, 3, 2, 15, arcade.color.AUBURN)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_lrtb_rectangle_filled(0, 30, 600, 0, arcade.color.BLACK)
-        arcade.draw_lrtb_rectangle_filled(0, 600, 30, 0, arcade.color.BLACK)
-        arcade.draw_lrtb_rectangle_filled(0, 600, 600, 570, arcade.color.BLACK)
-        arcade.draw_lrtb_rectangle_filled(570, 600, 600, 0, arcade.color.BLACK)
+        arcade.draw_lrtb_rectangle_filled(0, 30, 600, 0, arcade.color.LIME_GREEN)
+        arcade.draw_lrtb_rectangle_filled(0, 600, 30, 0, arcade.color.PINK)
+        arcade.draw_lrtb_rectangle_filled(0, 600, 600, 570, arcade.color.PINK)
+        arcade.draw_lrtb_rectangle_filled(570, 600, 600, 0, arcade.color.LIME_GREEN)
+        arcade.draw_lrtb_rectangle_filled(0, 30, 30, 0, arcade.color.BLACK)
+        arcade.draw_lrtb_rectangle_filled(0, 30, 600, 570, arcade.color.BLACK)
+        arcade.draw_lrtb_rectangle_filled(570, 600, 30, 0, arcade.color.BLACK)
+        arcade.draw_lrtb_rectangle_filled(570, 600, 600, 570, arcade.color.BLACK)
         self.ball.draw_ball()
 
     def on_update(self, dt):
