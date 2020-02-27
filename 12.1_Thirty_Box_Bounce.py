dee26378f1b297f3f0b5b30ac28af274e8d4afa6
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-02-27 13:44:43 -0600

    Name commit ;P

--- 12.1_Thirty_Box_Bounce.py
+++ 12.1_Thirty_Box_Bounce.py
@@ -4,10 +4,10 @@
 You will want to incorporate lists to modify the
 Ball Bounce Program to create the following:
 
-    1.) Screen size 600 x 600
-    2.) Draw four 30px wide side rails on all four sides of the window
-    3.) Make each side rail a different color.
-    4.) Draw 30 black boxes(squares) of random size from 10-50 pixels
+1.) Screen size 600 x 600
+2.) Draw four 30px wide side rails on all four sides of the window
+3.) Make each side rail a different color.
+4.) Draw 30 black boxes(squares) of random size from 10-50 pixels
 5.) Animate them starting at random speeds from -300 to +300 pixels/second. 
 6.) All boxes must be moving.
 7.) Start all boxes in random positions between the rails.
