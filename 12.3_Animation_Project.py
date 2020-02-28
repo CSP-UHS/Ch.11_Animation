b3d0b5c3521e95a0d18771094fca48be6e246d6e
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-02-28 15:09:44 -0600

    Name commit ;P

--- 12.3_Animation_Project.py
+++ 12.3_Animation_Project.py
@@ -7,10 +7,11 @@ import arcade
 import random
 
 class star:
-    def __init__(self, pos_x, pos_y, dy, col):
+    def __init__(self, pos_x, pos_y, dx, dy, col):
         self.radius = random.randint(1, 3)
         self.pos_x = pos_x
         self.pos_y = pos_y
+        elf.dx = dx
         self.dy = dy
         self.col = col
 
@@ -28,12 +29,19 @@ class MyGame(arcade.Window):
         for i in range(15):
             self.star = star(random.randint(0, 600), random.randint(600, 400), random.randint(-4, 1), arcade.color.BUBBLES)
             self.starlist.append(self.snow_ball)
+
     def on_draw(self):
         arcade.start_render()
         arcade.draw_lrtb_rectangle_filled(0,600,200,0, arcade.color.AO) # GRASS
         arcade.draw_triangle_filled(100, 200, 300, 400, 600, 200, arcade.color.EBONY) #middle mountian
         arcade.draw_triangle_filled(400, 200, 600, 350, 600, 200, arcade.color.DARK_GRAY)  # right mountian
         arcade.draw_triangle_filled(0,200, 0, 400, 500, 200, arcade.color.DIM_GRAY) #left mountian
+        # for item in self.starlist:
+        #     item.draw_ball()
+
+    # def on_update(self, dt):
+    #     for item in self.starlist:
+    #         item.update_ball()
 
 
 def main():
