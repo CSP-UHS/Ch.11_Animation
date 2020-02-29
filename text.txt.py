72462d1f2aa1bfd21dfb658fa59384cf29c17e7b
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-02-28 20:25:39 -0600

    Name commit ;P

--- text.txt.py
+++ text.txt.py
@@ -1,18 +1,48 @@
 import arcade
+import random
+
+# arcade.open_window(600,600, "lol")
+# arcade.set_background_color(arcade.color.WHITE)
+# arcade.start_render()
+#
+# a = 14
+# a2 = 20
+# my_list = (
+#     (a, a2),
+#     (18, 18),
+#     (14, 16),
+#     (12, 10),
+#     (10, 16),
+#     (6, 18),
+#     (10, 20),
+#     (12, 26),
+#     (14, 20),
+# )
+# arcade.draw_polygon_filled(my_list, arcade.color.BLACK)
+#
+# arcade.finish_render()
+# arcade.run()
 
 arcade.open_window(600,600, "lol")
 arcade.set_background_color(arcade.color.WHITE)
 arcade.start_render()
+
+a = random.randint(10,30)
+a2 = random.randint(10,31)
 my_list = (
-    (14, 20),
-    (18, 18,
-    (14, 16),
-    (12, 10),
-    (10, 16),
-    (6, 18),
-    (10, 20),
-    (12, 26),
-     (14, 20),)
+    (a, a2),
+    (a+2, a2-1),
+    (a, a2-2),
+    (a-1, a2-5),
+    (a-2, a2-2),
+    (a-4, a2-1),
+    (a-2, a2),
+    (a-1, a2+2),
+    (a, a2),
+)
 
+arcade.draw_polygon_filled(my_list, arcade.color.BLACK)
 
+arcade.finish_render()
+arcade.run()
 
