6b9980e5243b7fe99bb1ac11531ab04f79b31db2
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-02-28 19:24:27 -0600

    Name commit ;P

---
+++ text.txt.py
@@ -0,0 +1,18 @@
+import arcade
+
+arcade.open_window(600,600, "lol")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+my_list = (
+    (14, 20),
+    (18, 18,
+    (14, 16),
+    (12, 10),
+    (10, 16),
+    (6, 18),
+    (10, 20),
+    (12, 26),
+     (14, 20),)
+
+
+
