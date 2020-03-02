25549e85c8be674b755a038f4b7e15f913d84e0a
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-03-02 11:33:15 -0600

    Name commit ;P

--- 12.3_Animation_Project.py
+++ 12.3_Animation_Project.py
@@ -5,8 +5,10 @@ Your choice!!! Have fun and be creative.
 '''
 import arcade
 import random
+SH=600
+SW=600
 
-class star:
+class Star:
     def __init__(self, pos_x, pos_y, dx, dy, col):
         self.radius = random.randint(1, 3)
         self.pos_x = pos_x
@@ -15,11 +17,11 @@ class star:
         self.dy = dy
         self.col = col
 
-    def update_ball(self):
-        self.pos_y += self.dy
+    def update_star(self):
+        self.pos_y -= self.dy
         self.pos_x += self.dx
 
-    def draw_ball(self):
+    def draw_star(self):
         a = random.randint(0, 595)
         a2 = random.randint(400, 595)
         my_list = (
@@ -33,7 +35,6 @@ class star:
             (a - 1, a2 + 2),
             (a, a2),
         )
-
         arcade.draw_polygon_filled(my_list, arcade.color.BUBBLES)
 
 class MyGame(arcade.Window):
@@ -41,8 +42,8 @@ class MyGame(arcade.Window):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.CATALINA_BLUE)
         self.starlist = []
-        for i in range(100):
-            self.star = star(random.randint(0, 595), random.randint(400, 595),random.randint(-4,1),random.randint(-4,1), arcade.color.BUBBLES) #put in correct numbers
+        for i in range(1):
+            self.star = Star(random.randint(0, 595), random.randint(400, 595),1,1, arcade.color.BUBBLES) #put in correct numbers
             self.starlist.append(self.star)
 
     def on_draw(self):
@@ -52,16 +53,14 @@ class MyGame(arcade.Window):
         arcade.draw_triangle_filled(400, 200, 600, 350, 600, 200, arcade.color.DARK_GRAY)  # right mountian
         arcade.draw_triangle_filled(0,200, 0, 400, 500, 200, arcade.color.DIM_GRAY) #left mountian
         for item in self.starlist:
-            item.draw_ball()
+            item.draw_star()
 
     def on_update(self, dt):
         for item in self.starlist:
-            item.update_ball()
+            item.update_star()
 
 
 def main():
-    SH = 600
-    SW = 600
     window = MyGame(SH,SW, "SnowFall")
     arcade.run()
 main()
