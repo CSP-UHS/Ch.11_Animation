03806577a46d1dde1b945348a2d1df2c1fddd89b
nellieleaverton <leavertonnel@urbandaleschools.com>
2020-03-05 13:12:46 -0600

    Name commit ;P

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -13,7 +13,7 @@ All questions are about the final code in Chapter 12:
 5.) Which method is run 60 times/second?
     on_draw
 6.) What does this code do?   self.dx *= -1
-    change the x direction
+    change the x direction by -1 every 1/60 second
 7.) What does this code do?  self.pos_y += self.dy
     the y position is now set to the y direction
 8.) What is the width of the window?
