923112fd558822057431da19c3f70816b4c7def5
watsoncal <watsoncal@urbandaleschools.com>
2020-03-06 13:39:36 -0600

    Finished Jedi Training #12

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -1,27 +1,27 @@
 '''
-Sign your name:________________
+Sign your name: Cal Watson
  
 All questions are about the final code in Chapter 12:
 
-1.) Where is the ball's original location?
+1.) Where is the ball's original location? (320,240)
 
-2.) What are the variables dx and dy?
+2.) What are the variables dx and dy? horizontal (x-direction) and vertical (y-direction) velocities
 
-3.) How many pixels/sec does the ball move in the x-direction?
+3.) How many pixels/sec does the ball move in the x-direction? 180 pixels/sec
 
-4.) How many pixels/sec does the ball move in the y-direction?
+4.) How many pixels/sec does the ball move in the y-direction? 120 pixels/sec
 
-5.) Which method is run 60 times/second?
+5.) Which method is run 60 times/second? on_update
 
-6.) What does this code do?   self.dx *= -1
+6.) What does this code do?   self.dx *= -1     changes the x-direction of the ball
 
-7.) What does this code do?  self.pos_y += self.dy
+7.) What does this code do?  self.pos_y += self.dy     changes the position of the ball in the y-direction
 
-8.) What is the width of the window?
+8.) What is the width of the window? 640 pixels
 
-9.) What is this code checking?  self.pos_y > SH - self.rad:
+9.) What is this code checking?  self.pos_y > SH - self.rad:     if the radius of the ball in the y-direction is off the screen
 
-10.) What is this code checking? if self.pos_x < self.rad
+10.) What is this code checking? if self.pos_x < self.rad     if the position of the ball in the x-direction is partially off of the screen
 
 
 
