9e982104f8d833cd2eed7c9164fd89bf8dfa993b
floryken <floryken@urbandaleschools.com>
2020-03-06 13:31:25 -0600

    a committ

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -1,28 +1,26 @@
 '''
-Sign your name:________________
+Sign your name:Kenny Flory
  
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+320,240
 2.) What are the variables dx and dy?
-
+speed it moves in a direction
 3.) How many pixels/sec does the ball move in the x-direction?
-
+180
 4.) How many pixels/sec does the ball move in the y-direction?
-
+120
 5.) Which method is run 60 times/second?
-
+on_update
 6.) What does this code do?   self.dx *= -1
-
+inverts the number of the x to make it so the ball doesn't go off screen
 7.) What does this code do?  self.pos_y += self.dy
-
+moves ball up at the speed of dy
 8.) What is the width of the window?
-
+640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+if the ball is hitting the edge of the screen on the bottom
 10.) What is this code checking? if self.pos_x < self.rad
-
-
-
+if ball is hitting the edge of the screen on the left
 '''
