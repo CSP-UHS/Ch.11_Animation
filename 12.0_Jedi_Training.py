fad5dc43b5645720d583b1283e94444688f5de3d
getzbry <getzbry@urbandaleschools.com>
2021-03-09 09:46:49 -0600

    done

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -4,24 +4,25 @@ Sign your name:________________
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+320, 240
 2.) What are the variables dx and dy?
-
+velocity
 3.) How many pixels/sec does the ball move in the x-direction?
-
+3
 4.) How many pixels/sec does the ball move in the y-direction?
-
+-2
 5.) Which method is run 60 times/second?
-
+on_update
 6.) What does this code do?   self.dx *= -1
-
+it changes the velocity of the ball
 7.) What does this code do?  self.pos_y += self.dy
-
+its where the ball is supposed to be drawn
 8.) What is the width of the window?
-
+640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+to see if the ball is still within the top of the window or not.
 10.) What is this code checking? if self.pos_x < self.rad
+to see if the ball is still within the left side of the window or not.
 
 
 
