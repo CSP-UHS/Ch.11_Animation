8d5ee4d806ebe2f6796cf28761bdcc3fef985a3f
Geni W <williamsgen@urbandaleschools.com>
2021-03-11 10:30:45 -0600

    30 boxes

--- 12.1_Thirty_Box_Bounce.py
+++ 12.1_Thirty_Box_Bounce.py
@@ -27,17 +27,82 @@ import random
 
 SW = 600
 SH = 600
-BN = 30  # Number of boxes
+box_num = 30
+BW = 30
 
 
 class Box:
-    def __init__(self, x, y, dx, dy, r, c):  # x position, y position, dx & dy, radius, and color
+    def __init__(self, x, y, side, dx, dy, c):
         self.x = x
         self.y = y
+        self.side = side
         self.dx = dx
         self.dy = dy
-        self.r = r
         self.c = c
 
     def draw_box(self):
-        arcade.draw_rectangle_filled(self.x, self.y, self.r, self.c)
+        arcade.draw_rectangle_filled(self.x, self.y, self.side, self.side, self.c)
+
+    def update_box(self):
+        self.x += self.dx
+        self.y += self.dy
+
+        # Bounce off left
+        if self.x <= BW + self.side / 2:
+            self.dx *= -1
+            self.c = arcade.color.PINK
+        # Bounce off right
+        if self.x >= SW - BW - self.side / 2:
+            self.dx *= -1
+            self.c = arcade.color.BANANA_YELLOW
+        # Bounce off top
+        if self.y >= SH - BW - self.side / 2:
+            self.dy *= -1
+            self.c = arcade.color.BLUE_GREEN
+        # Bounce off bottom
+        if self.y <= BW + self.side / 2:
+            self.dy *= -1
+            self.c = arcade.color.BLUEBERRY
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.WHITE)
+        self.box_list = []  # Make list to hold boxes
+
+        # create boxes
+        for i in range(box_num):
+            s = random.randint(10, 50)
+            dx = random.randint(-5, 5)
+            dy = random.randint(-5, 5)
+            x = random.randint(BW + s // 2, SW - BW - s // 2)
+            y = random.randint(BW + s // 2, SW - BW - s // 2)
+            c = arcade.color.BLACK
+            if dx == 0 and dy == 0:
+                dx = 20
+            box = Box(x, y, s, dx, dy, c)
+            self.box_list.append(box)
+
+    def on_draw(self):
+        arcade.start_render()
+        for box in self.box_list:
+            box.draw_box()
+        # Make rails
+        arcade.draw_rectangle_filled(BW // 2, SH // 2, BW, SH, arcade.color.PINK)  # Left
+        arcade.draw_rectangle_filled(SW - BW // 2, SH // 2, BW, SH, arcade.color.BANANA_YELLOW)  # Right
+        arcade.draw_rectangle_filled(SW // 2, SH - BW // 2, SW, BW, arcade.color.BLUE_GREEN)  # Top
+        arcade.draw_rectangle_filled(SW // 2, BW // 2, SW, BW, arcade.color.BLUEBERRY)  # Bottom
+
+    def on_update(self, dt):
+        for box in self.box_list:
+            box.update_box()
+
+
+def main():
+    window = MyGame(SW, SH, "30 Boxes")
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
