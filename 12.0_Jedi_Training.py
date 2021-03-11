cc54bf2c0b691007897764f276ff872df7e4046a
caltrideraar <caltrideraar@urbandaleschools.com>
2021-03-11 10:28:45 -0600

    Thirty Box done

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -1,23 +1,23 @@
 '''
-Sign your name:________________
+Sign your name: Aaron Caltrider
  
 All questions are about the final code in Chapter 12:
 
-1.) Where is the ball's original location?
+1.) Where is the ball's original location? 320, 240
 
 2.) What are the variables dx and dy?
 
-3.) How many pixels/sec does the ball move in the x-direction?
+3.) How many pixels/sec does the ball move in the x-direction? 180
 
-4.) How many pixels/sec does the ball move in the y-direction?
+4.) How many pixels/sec does the ball move in the y-direction? -120
 
-5.) Which method is run 60 times/second?
+5.) Which method is run 60 times/second? on_update
 
-6.) What does this code do?   self.dx *= -1
+6.) What does this code do?   self.dx *= -1 Switches sign on velocity.
 
-7.) What does this code do?  self.pos_y += self.dy
+7.) What does this code do?  self.pos_y += self.dy Adds velocity onto position of ball.
 
-8.) What is the width of the window?
+8.) What is the width of the window? 640
 
 9.) What is this code checking?  self.pos_y > SH - self.rad:
 
--- 12.1_Thirty_Box_Bounce.py
+++ 12.1_Thirty_Box_Bounce.py
@@ -22,3 +22,95 @@ Helpful Hints:
 4.) Also in the on_draw section draw the side rails.
 5.) In the on_update section use: for box in self.boxlist: box.update_box()
 '''
+
+
+import arcade
+import random
+SW = 600
+SH = 600
+box_num = 30
+bw = 30
+
+class Box:
+    def __init__(self, x, y, side, dx, dy, c):
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.side = side
+        self.c = c
+
+    def draw_box(self):
+        arcade.draw_rectangle_filled(self.x, self.y, self.side, self.side, self.c)
+
+    def update_box(self):
+        self.x += self.dx
+        self.y += self.dy
+
+        #Bounce off left
+        if self.x <= bw+self.side/2:
+            self.dx *=-1
+            self.c = arcade.color.RED
+        #Bounce off right
+        if self.x >= SW - bw - self.side / 2:
+            self.dx *= -1
+            self.c = arcade.color.YELLOW
+
+        #Bounce off top
+        if self.y >= SH - bw - self.side / 2:
+            self.dy *= -1
+            self.c = arcade.color.GREEN
+
+        #Bounce off bottom
+        if self.y <= bw + self.side / 2:
+            self.dy *= -1
+            self.c = arcade.color.BLUE
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.WHITE)
+
+        self.box_list=[] # Create list to hold all boxes
+
+        #Create our boxes
+        for i in range(box_num):
+            s = random.randint(10,50)
+            dx = random.randint(-5, 5)
+            dy = random.randint(-5, 5)
+            x = random.randint(bw+s//2, SW-bw-s//2)
+            y = random.randint(bw + s // 2, SH - bw - s // 2)
+            c = arcade.color.BLACK
+            if dx==0 and dy==0:
+                dx = 20
+
+
+            box = Box(x, y, s, dx, dy, c)
+            self.box_list.append(box)
+
+
+
+    def on_draw(self):
+        arcade.start_render()
+        for box in self.box_list:
+            box.draw_box()
+
+        # Make Rails
+        arcade.draw_rectangle_filled(bw//2,SH//2,bw,SH,arcade.color.RED)
+        arcade.draw_rectangle_filled(SW-bw//2,SH//2,bw,SH,arcade.color.YELLOW)
+        arcade.draw_rectangle_filled(SW//2,SH-bw//2,SW,bw,arcade.color.GREEN)
+        arcade.draw_rectangle_filled(SW//2,bw//2,SW,bw,arcade.color.BLUE)
+
+
+
+    def on_update(self, dt):
+        for box in self.box_list:
+            box.update_box()
+
+def main():
+    window = MyGame(SW,SH,"30 Boxes")
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
+
