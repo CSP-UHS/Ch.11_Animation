4f979d2676c60f1e6882849ffd048d4bd3235655
MitchellDan <mitchelldan@urbandalescools.com>
2021-03-21 22:50:17 -0500

    did t h i n g

--- 12.3_Animation_Project.py
+++ 12.3_Animation_Project.py
@@ -1,19 +1,5 @@
 '''
-SNOWFALL
---------
-Try to create the snowfall animation by meeting
-the following requirements:
 
-1.) Create a 600 x 600 window with black background
-2.) Window title equals "Snowfall"
-3.) Crossbars 10 px wide. Snow must be outside!
-4.) Make snowflake radius random between 1-3
-5.) Randomly start snowflakes anywhere in the window.
-6.) Random downward speed of -4 to -1
-7.) Start snowflakes again at random x from 0-600 and random y from 600-700
-8.) Generate 300 snowflakes
-9.) Color snowflake #1 red just for fun.
-10.) All other snowflakes should be white.
 
 
 '''
@@ -22,13 +8,14 @@ import arcade
 import random
 import time
 
-SW = 600
-SH = 600
+SW = 900
+SH = 900
 snowflakes = 100
 
 
 class Wind0s():
-    def __init__(self, x, y, dx, dy, w, h, c, t, v,cond): #x,y velx, vely, width/radius,height,color, title, identifyer, ident 2
+    def __init__(self, x, y, dx, dy, w, h, c, t, v,
+                 cond):  # x,y vel x, vel y, width/radius, height, color, title, identifier, ident 2
         self.x = x
         self.y = y
         self.dx = dx
@@ -39,85 +26,120 @@ class Wind0s():
         self.t = t
         self.v = v
         self.window = cond
+        self.prog = 0
+        self.curx = 300
+        self.cury = 300
 
-    def open(self):
+    def open(self):  # called to open window
         # print("woah")
         if self.window == False:
             self.window = True
         else:
             self.window = False
+            self.prog = 0  # reset animation
 
-
-    #def push(self, cx, x, y):
+    # def push(self, cx, x, y):
     #    if abs(self.x - x) <= 40 and abs(self.y - y) <= 40:
     #        self.dx = self.dx + cx
 
-    #def hold(self, x, y, dx, dy):
-    #    if abs(self.x - x) <= 40 and abs(self.y - y) <= 40:
-    #        self.x = self.x + dx
-    #        self.y = self.y + dy
-    #        self.y = self.y + self.dy
+    def hold(self, x, y, dx, dy): # Moves the windows and any objects in the windows "home point" with the mouse drag.
+        # if abs(self.x - x) <= 40 and abs(self.y - y) <= 40:
+        self.curx = self.curx + dx
+        self.cury = self.cury + dy
 
+    # This does wayyyy more than draw circles but im to lazy to rename it. Responsible for drawing everything on screen. CAUTION, Held together with magic and tape so yeah.
     def drawcircle(self):
 
-        arcade.draw_rectangle_filled(300,595,600,15,arcade.color.BLUE)
-        arcade.draw_text("Wind0s V.4.20.69", 25 , 585, arcade.color.WHITE, 12, 0, "center")
-        arcade.draw_text(str(time.ctime()), 400, 585, arcade.color.WHITE, 12, 0, "center")
+        arcade.draw_rectangle_filled((0.5 * SW), (SH - 5), SW, 15, arcade.color.BLUE)  # Header
+        arcade.draw_text("Wimd0s V.4.20.69", 25, (SH - 15), arcade.color.WHITE, 12, 0, "center")
+        arcade.draw_text(str(time.ctime()), (SW - 160), (SH - 15), arcade.color.WHITE, 12, 0, "center")
 
-        if self.v == "na":
+        if self.v == "na": #Icons on desktop
             arcade.draw_circle_filled(self.x, self.y, 15, self.color)
-            arcade.draw_text(self.t, self.x -50, self.y - 30, arcade.color.WHITE, 12, 0, "center")
+            arcade.draw_text(self.t, self.x - 50, self.y - 30, arcade.color.WHITE, 12, 0, "center")
 
         if self.window == True:
-            if self.v == "na": #if it has no identify do this
-                arcade.draw_rectangle_filled(300,300,self.w,self.h,arcade.color.WHITE)
-                arcade.draw_rectangle_filled(300,550,500,20,self.color)
-                arcade.draw_text(self.t,80,540,arcade.color.WHITE_SMOKE,12)
-                arcade.draw_rectangle_outline(300,305,self.w,self.h+10,arcade.color.COOL_GREY)
-
-            if self.t =="Mozzerella Icecat": #mozz code lol
-                off = 20
-                arcade.draw_text("G",200+off,380,arcade.color.BLUE,30,0,"left")  #Draw Goongle logo
-                arcade.draw_text("O0", 220+off, 380, arcade.color.DARK_GREEN, 30, 0, "left")
-                arcade.draw_text("N", 260+off, 380, arcade.color.RED, 30, 0, "left")
-                arcade.draw_text("G", 280+off, 380, arcade.color.GREEN, 30, 0, "left")
-                arcade.draw_text("L", 300+off, 380, arcade.color.YELLOW, 30, 0, "left")
-                arcade.draw_text("E", 320+off, 380, arcade.color.ORANGE, 30, 0, "left")
-
-                arcade.draw_rectangle_outline(300,360,200,30,arcade.color.CADET_GREY) #Search Bar
-                arcade.draw_rectangle_filled(400,360,30,30,arcade.color.BLUE)
-                arcade.draw_circle_outline(404,364,7.5,arcade.color.COOL_GREY,3)
-                arcade.draw_rectangle_filled(396,356,12,4,arcade.color.COOL_GREY,135)
-
-            if self.t == "      Snowfall": #snowfall code
-                #print(self.x)
-                if self.v =="sf":
-                    #print("bruh")
-                    arcade.draw_circle_filled(self.x, self.y, self.w, arcade.color.RED)
+            if self.v == "na":  # if window has no identify do this (draws blank window"
+                # print("test")
+                if self.prog <= 500:
+                    arcade.draw_rectangle_outline(self.curx, self.cury, self.prog, self.prog, arcade.color.COOL_GREY)
+                if self.prog >= 500:
+                    arcade.draw_rectangle_filled(self.curx, self.cury, self.w, self.h, arcade.color.WHITE)
+                    arcade.draw_rectangle_filled(self.curx, self.cury + 250, 500, 20, self.color)
+                    arcade.draw_text(self.t, self.curx - 230, self.cury + 240, arcade.color.WHITE_SMOKE, 12)
+                    arcade.draw_rectangle_outline(self.curx, self.cury + 5, self.w, self.h + 10, arcade.color.COOL_GREY)
 
 
+            if self.prog >= 500:
+                if self.t == "Mozzerella Icecat":  # mozz code
 
+                    # Draw Goongle logo
+                    off = 20
+                    arcade.draw_text("G", self.curx - 100 + off, self.cury + 80, arcade.color.BLUE, 30, 0, "left")
+                    arcade.draw_text("O0", self.curx - 80 + off, self.cury + 80, arcade.color.DARK_GREEN, 30, 0, "left")
+                    arcade.draw_text("N", self.curx - 40 + off, self.cury + 80, arcade.color.RED, 30, 0, "left")
+                    arcade.draw_text("G", self.curx - 20 + off, self.cury + 80, arcade.color.GREEN, 30, 0, "left")
+                    arcade.draw_text("L", self.curx + off, self.cury + 80, arcade.color.YELLOW, 30, 0, "left")
+                    arcade.draw_text("E", self.curx + 20 + off, self.cury + 80, arcade.color.ORANGE, 30, 0, "left")
+
+                    arcade.draw_rectangle_outline(self.curx, self.cury + 60, 200, 30,
+                                                  arcade.color.CADET_GREY)  # Search Bar
+                    arcade.draw_rectangle_filled(self.curx + 100, self.cury + 60, 30, 30, arcade.color.BLUE)
+                    arcade.draw_circle_outline(self.curx + 104, self.cury + 64, 7.5, arcade.color.COOL_GREY, 3)
+                    arcade.draw_rectangle_filled(self.curx + 96, self.cury + 65, 12, 4, arcade.color.COOL_GREY, 135)
+
+                if self.t == "      Snowfall":  # snowfall code
+                    # print(self.v)
+                    if self.v == "sf":
+                        # print("bruh")
+                        arcade.draw_circle_filled(self.x + (self.curx - 300), self.y + (self.cury - 300), self.w,
+                                                  arcade.color.BLACK)
+
+                if self.t == "      Bounce":  # Bounce code
+                    # print(self.v)
+                    if self.v == "ball":
+                        # print(self.v)
+                        arcade.draw_circle_filled((self.x + (self.curx - 300)), (self.y + (self.cury - 300)), self.w,
+                                                  self.color)  # draws ball
 
     def updatecirlce(self):
 
-
-        self.x += self.dx
-        self.y += self.dy
-
-        if self.v == "sf":
+        if self.v == "sf" and self.v != "ball":  # movement for snowflakes
+            self.y = self.y + self.dy  # + (0 * (self.cury - 300))
             self.dx = (self.dx + (random.randint(-1, 1)) * 0.1)
             if abs(self.dx) > 2:
                 self.dx = self.dx * 0.5
-            self.x += self.dx
+            self.x += (self.dx)  # + (0* (self.curx - 300))
+            # print(self.curx)
 
-            if self.y <= 50:
-                self.y = 540
-            if self.y >= 701:
-                self.y = random.randint(-100, 0)
-            if self.x > 545:
-                self.x = 55
-            if self.x < 55:
-                self.x = 545
+            if self.y <= (50):  # + (self.cury - 300)):
+                self.y = (540)
+            if self.y >= (701):  # + (self.cury - 300)):
+                self.y = (random.randint(-100, 0))  # + (self.cury - 300))
+            if self.x > (545):  # + (self.curx - 300)):
+                self.x = (55)
+            if self.x < (55):  # + (self.curx - 300)):
+                self.x = (545)
+
+        if self.v == "ball" and self.v != "sf":  # movement for balls
+            self.dx = self.dx
+            self.dy = self.dy - (9.81 / 60)
+            self.x += self.dx
+            self.y += self.dy
+
+            if self.x <= 65 or self.x >= 535:
+                self.dx = (self.dx * -1)
+                # self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+            if self.y >= 535 or self.y <= 75:
+                self.dy = (self.dy * -1) + (9.81 / 60)
+                # self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+            if self.y <= 5:
+                self.y = 16
+
+        if self.window == True:  # window open animation
+            self.prog = self.prog + 50
 
 
 class MyGame(arcade.Window):
@@ -125,24 +147,37 @@ class MyGame(arcade.Window):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.BLACK)
 
-        #Snowflakes Code
+        # Snowflakes Code
         flake_list = []
-        for i in range(100):
-            self.flake = Wind0s(random.randint(65, 525), random.randint(65, 525), 0, (-1 * random.randint(1, 4)), random.randint(1, 3),0, arcade.color.WHITE,"      Snowfall","sf",False)
+        for i in range(30):
+            self.flake = Wind0s(random.randint(65, 525), random.randint(65, 525), 0, (-1 * random.randint(1, 4)),
+                                random.randint(1, 3), 0, arcade.color.WHITE, "      Snowfall", "sf", False)
             flake_list.append(self.flake)
             self.flake_list = flake_list
-        #print(self.flake_list)
+        # print(self.flake_list)
 
-        self.window_list = [] #OVERALL WINDOWS LIST
+        # OVERALL WINDOWS LIST (Makes "Shortcut" and defines some parameters of the window once its open)
+        self.window_list = []
 
-        self.window = Wind0s(50, 40, 0, 0, 500, 500, arcade.color.PURPLE, "Mozzerella Icecat","na",False)
+        self.window = Wind0s(50, 40, 0, 0, 500, 500, arcade.color.PURPLE, "Mozzerella Icecat", "na", False)
         self.window_list.append(self.window)
-        self.window = Wind0s(50, 85, 0, 0, 500, 500, arcade.color.BLUE, "      Test Test","na",False)
+        self.window = Wind0s(50, 85, 0, 0, 500, 500, arcade.color.BLUE, "      Test Test", "na", False)
         self.window_list.append(self.window)
-        self.window = Wind0s(50, 130, 0, 0, 500, 500, arcade.color.BRICK_RED, "      Snowfall","na",False)
+        self.window = Wind0s(50, 130, 0, 0, 500, 500, arcade.color.BRICK_RED, "      Snowfall", "na", False)
         self.window_list.append(self.window)
-        self.window = Wind0s(50, 175, 0, 0, 500, 500, arcade.color.DARK_GREEN, "      Bounce","na",False)
+        self.window = Wind0s(50, 175, 0, 0, 500, 500, arcade.color.DARK_GREEN, "      Bounce", "na", False)
         self.window_list.append(self.window)
+        # self.window = Wind0s(50, 220, 0, 0, 500, 500, arcade.color.BANANA_MANIA, "      Hermon", "na", False)
+        # self.window_list.append(self.window)
+
+        # Bounce This if for the actual balls in the window, defines starting x and y as well as velocitys, radius and colors
+        ball_list = []
+        self.ball = Wind0s(280, 500, 3, 0, 15, 0, arcade.color.BLUE, "      Bounce", "ball", False)
+        ball_list.append(self.ball)
+        self.ball = Wind0s(320, 500, -3, 0, 15, 0, arcade.color.BLUE, "      Bounce", "ball", False)
+        ball_list.append(self.ball)
+        self.ball_list = ball_list
+        #print(ball_list)
 
     def on_draw(self):
         arcade.start_render()
@@ -151,23 +186,30 @@ class MyGame(arcade.Window):
             box.drawcircle()
 
         for flake in self.flake_list:
-            flake.drawcircle()
+            if flake.window == True:
+                flake.drawcircle()
 
-
-
-
+        for ball in self.ball_list:
+            if ball.window == True:
+                ball.drawcircle()
 
     def on_update(self, dt):
 
-        for flake in self.flake_list:
-            flake.updatecirlce()
-
         for box in self.window_list:
-            box.updatecirlce()
+            if box.window == True:
+                box.updatecirlce()
 
+        for flake in self.flake_list:
+            if flake.window == True:
+                flake.updatecirlce()
+
+        for ball in self.ball_list:
+            if ball.window == True:
+                # print("balls")
+                ball.updatecirlce()
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
-        print("Open")
+        # print("Open")
         for flake in self.window_list:
             if abs(x - flake.x) <= 15 and abs(y - flake.y) <= 15:
                 flake.open()
@@ -176,18 +218,41 @@ class MyGame(arcade.Window):
             if abs(x - 50) <= 15 and abs(y - 130) <= 15:
                 flake.open()
 
+        for flake in self.ball_list:
+            if abs(x - 50) <= 15 and abs(y - 175) <= 15:
+                # print("hrrng")
+                flake.open()
 
     # def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
     #    for flake in self.window_list:
     #        flake.push(dy, x, y)
+    # So there is a lot going on here, this sends paramiters to the "hold" function in the WinD0s class. It looks for where the mouse is and sends that and the mouses movememt to hte hold function.
+    def on_mouse_drag(self, x: float, y: float, dx: float, dy: float, buttons: int, modifiers: int):
+        # print("test")
+        for box in self.window_list:  # For dragging windows around
+            if box.window == True:
+                if y - box.cury >= 235 and y - box.cury <= 265:
+                    if x - box.curx >= -250 and x - box.curx <= 250:
+                        # print("test pass")
+                        box.hold(x, y, dx, dy)
 
-    #def on_mouse_drag(self, x: float, y: float, dx: float, dy: float, buttons: int, modifiers: int):
-    #    for flake in self.flake_list:
-    #        flake.hold(x, y, dx, dy)
+        for ball in self.ball_list:  # for the movement of the balls in the window while dragging
+            if ball.window == True:
+                if y - ball.cury >= 235 and y - ball.cury <= 265:
+                    if x - ball.curx >= -250 and x - ball.curx <= 250:
+                        # print("test pass")
+                        ball.hold(x, y, dx, dy)
+
+        for flake in self.flake_list: # for the movement of the snowflakes in the window while dragging
+            if flake.window == True:
+                if y - flake.cury >= 235 and y - flake.cury <= 265:
+                    if x - flake.curx >= -250 and x - flake.curx <= 250:
+                        # print("test pass")
+                        flake.hold(x, y, dx, dy)
 
 
 def main():
-    mywindow = MyGame(SW, SH, "Snowfall")
+    mywindow = MyGame(SW, SH, "Wimd0s")
     arcade.run()
 
 
