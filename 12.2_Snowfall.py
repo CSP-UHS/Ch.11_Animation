5358721e40bfc491e51955ab087015c28d4859f7
caltrideraar <caltrideraar@urbandaleschools.com>
2021-03-22 10:03:48 -0500

    Snowfall done

--- 12.2_Snowfall.py
+++ 12.2_Snowfall.py
@@ -17,3 +17,79 @@ the following requirements:
 
 
 '''
+
+import arcade
+import random
+SW = 600
+SH = 600
+FN = 300
+
+class Flake:
+    def __init__(self, x, y, dy, r, c):
+        self.x = x
+        self.y = y
+        self.dy = dy
+        self.r = r
+        self.c = c
+
+    def draw_flake(self):
+        arcade.draw_circle_filled(self.x, self.y, self.r, self.c)
+
+    def update_flake(self):
+        self.y += self.dy
+
+        # Respawn flakes above window
+        if self.y <= -self.r:
+            self.y = random.randint(SH,SH+100)
+            self.x = random.randint(0,SW)
+
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.BLACK)
+
+        self.flake_list=[] # Create list to hold all snow flakes
+
+        # create our flakes
+        for i in range(FN):
+            r = random.randint(1,4)
+            dy = random.randint(-4, -1)
+            x = random.randint(0,SW)
+            y = random.randint(0,SH)
+
+            if i==1:
+                c = arcade.color.RED
+            else:
+                c = arcade.color.RED
+
+
+            makeflake = Flake(x, y, dy, r, c)
+            self.flake_list.append(makeflake)
+
+
+
+    def on_draw(self):
+        arcade.start_render()
+        for flake in self.flake_list:
+            flake.draw_flake()
+
+        # Make window pane
+        arcade.draw_rectangle_filled(SW//2,SH//2, 10,SH,arcade.color.ALLOY_ORANGE)
+        arcade.draw_rectangle_filled(SW // 2, SH // 2, SW, 10, arcade.color.ALLOY_ORANGE)
+
+
+
+
+    def on_update(self, dt):
+        for flake in self.flake_list:
+            flake.update_flake()
+
+def main():
+    window = MyGame(SW,SH,"Snowfall")
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
+
