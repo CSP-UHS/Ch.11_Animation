61d87591595ad6e8c949fd5473e2f90904eb9a15
MitchellDan <70217800+MitchellDan@users.noreply.github.com>
2021-03-23 10:43:33 -0500

    Update 12.0_Jedi_Training.py

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -1,28 +1,28 @@
 '''
-Sign your name:________________
+Sign your name:Daniel Michell
  
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+        (320, 240)
 2.) What are the variables dx and dy?
-
+        Velocity in pixesls
 3.) How many pixels/sec does the ball move in the x-direction?
-
+        180 pixels / sec
 4.) How many pixels/sec does the ball move in the y-direction?
-
+        120 pixels/sec
 5.) Which method is run 60 times/second?
-
+        On_update
 6.) What does this code do?   self.dx *= -1
-
+        makes the x velocity the oppposite of what it is
 7.) What does this code do?  self.pos_y += self.dy
-
+        the position equals its slef plus dy
 8.) What is the width of the window?
-
+        640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+        seeing if the ball is hitting the top
 10.) What is this code checking? if self.pos_x < self.rad
-
+        see if ball is hittig the left side
 
 
 '''
