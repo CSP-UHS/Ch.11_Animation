87bcbb6be989859749da097a97f22694460d18f4
waltersjac <waltersjac@urbandaleschools.com>
2021-03-25 09:31:08 -0500

    1st test commit

--- 12.3_Animation_Project.py
+++ 12.3_Animation_Project.py
@@ -9,41 +9,44 @@ import random
 SW=600
 SH=400
 
-class Ball:
-    def __init__(self,x,y,dx,dy,r,c):
+class Plane:
+    def __init__(self,x,y,dx,dy,w,c):
         self.x=x
         self.y=y
         self.dx=dx
         self.dy=dy
-        self.r=r
+        self.w=w
         self.c=c
 
-    def draw_ball(self):
-        arcade.draw_circle_filled(self.x,self.y,self.r,self.c)
+    def draw_plane(self):
+        body_list = (self.x, self.y), (self.x - 20, self.y + 10), (self.x - 55, self.y + 20), (self.x - 120, self.y +
+        20),(self.x - 150, self.y + 40), (self.x - 160, self.y + 40), (self.x - 160, self.y + 10), (self.x - 130, self.y - 5)\
+        , (self.x - 20, self.y - 5)
+        cockpit_list = (self.x - 20, self.y + 10), (self.x - 55, self.y + 10), (self.x - 55, self.y + 20)
+        wing_list = (self.x - 60, self.y - 3), (self.x - 90, self.y), (self.x - 110, self.y + 8), (self.x - 120, self.y + 8),\
+                    (self.x - 130, self.y - 3), (self.x - 60, self.y - 3)
+        arcade.draw_polygon_filled(body_list, arcade.color.ARMY_GREEN)
+        arcade.draw_polygon_filled(cockpit_list, arcade.color.LIGHT_BLUE)
+        arcade.draw_polygon_filled(wing_list, arcade.color.DESERT)
 
-    def update_ball(self):
+    def update_plane(self):
         self.x+= self.dx
         self.y+= self.dy
-
-        if self.x <= self.r or self.x >= SW - self.r:
-            self.dx *= -1
-        if self.y <= self.r or self.y >= SH - self.r:
-            self.dy *= -1
 
 
 
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width,height, title)
-        arcade.set_background_color(arcade.color.WHITE)
-        self.ball = Ball(320,240,3,-2,15,arcade.color.ARMY_GREEN)
+        arcade.set_background_color(arcade.color.BLACK)
+        self.plane = Plane(-40,200,3,0,20,arcade.color.ARMY_GREEN)
 
     def on_draw(self):
         arcade.start_render()
-        self.ball.draw_ball()
+        self.plane.draw_plane()
 
     def on_update(self, dt):
-        self.ball.update_ball()
+        self.plane.update_plane()
 
 def main():
     window = MyGame(SW, SH, "Window")
