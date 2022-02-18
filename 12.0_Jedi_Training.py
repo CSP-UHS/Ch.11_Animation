93d3ef4430a1bd48f10109d48038eb831d4dd2ae
clarksongav <ClarksonGav@urbandaleschools.com>
2022-02-18 13:00:30 -0600

    spop

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -4,24 +4,25 @@ Sign your name:________________
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+320, 240
 2.) What are the variables dx and dy?
-
+velocity
 3.) How many pixels/sec does the ball move in the x-direction?
-
+60
 4.) How many pixels/sec does the ball move in the y-direction?
-
+60
 5.) Which method is run 60 times/second?
-
+on_update
 6.) What does this code do?   self.dx *= -1
-
+make ball bounce off wall
 7.) What does this code do?  self.pos_y += self.dy
-
+its changing the position
 8.) What is the width of the window?
-
+640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+if the ball hits the top wall
 10.) What is this code checking? if self.pos_x < self.rad
+if the ball hits the left
 
 
 
