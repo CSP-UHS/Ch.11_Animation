ecb2edf3e49f544cc6c132fd12aabf67d5e64a06
jacobsonwil <jacobsonwil@urbandaleschools.com>
2022-02-21 17:53:29 -0600

    ch.12 commit

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -1,28 +1,28 @@
 '''
-Sign your name:________________
+Sign your name:Will Jacobson
  
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+Right in the middle of the screen
 2.) What are the variables dx and dy?
-
+velocity of x and y
 3.) How many pixels/sec does the ball move in the x-direction?
-
+180
 4.) How many pixels/sec does the ball move in the y-direction?
-
+120
 5.) Which method is run 60 times/second?
-
+on_update
 6.) What does this code do?   self.dx *= -1
-
+bounces the ball in the opposite direction
 7.) What does this code do?  self.pos_y += self.dy
-
+moves the object to the right or left
 8.) What is the width of the window?
-
+640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+is the edge of the ball touching the top of the window
 10.) What is this code checking? if self.pos_x < self.rad
-
+is the edge of the ball touching the side of the window
 
 
 '''
--- 12.1_Thirty_Box_Bounce.py
+++ 12.1_Thirty_Box_Bounce.py
@@ -22,3 +22,76 @@ Helpful Hints:
 4.) Also in the on_draw section draw the side rails.
 5.) In the on_update section use: for box in self.boxlist: box.update_box()
 '''
+import arcade
+import random
+
+SW = 600
+SH = 600
+
+class Box():
+    def __init__(self,pos_x,pos_y,dx,dy,rad,col):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.dx = dx
+        self.dy = dy
+        self.rad = rad
+        self.col = col
+    def draw_box(self):
+        arcade.draw_rectangle_filled(self.pos_x,self.pos_y,self.rad,self.rad,self.col)
+    def update_box(self):
+        self.pos_x += self.dx
+        self.pos_y += self.dy
+
+        #bounce ball off of walls
+        if self.pos_x > SW - self.rad:
+            self.dx *= -1
+            self.col = arcade.color.YELLOW
+        elif self.pos_x < self.rad:
+            self.dx *= -1
+            self.col = arcade.color.RED
+        elif self.pos_y > SH - self.rad:
+            self.dy *= -1
+            self.col = arcade.color.GREEN
+        elif self.pos_y < self.rad:
+            self.dy *= -1
+            self.col = arcade.color.BLUE
+
+class MyGame(arcade.Window):
+    def __init__(self,width,height,title):
+        super().__init__(width,height,title)
+        arcade.set_background_color(arcade.color.WHITE)
+        self.boxes = []
+        for i in range(30):
+            r = random.randint(10,50)
+            x = random.randint(30 + r, 570 - r)
+            y = random.randint(30 + r, 570 - r)
+            dx = random.randint(-5,5)
+            dy = random.randint(-5,5)
+            c = arcade.color.BLACK
+            if dx == 0 and dy == 0:
+                dx = 1
+                dy = 1
+            if dx == 0:
+                dx = 1
+            if dy == 0:
+                dy = 1
+            self.box = Box(x,y,dx,dy,r,c)
+            self.boxes.append(self.box)
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_rectangle_filled(SW / 2, 15, SW - 60, 30, arcade.color.BLUE)
+        arcade.draw_rectangle_filled(SW / 2, SH - 15, SH - 60, 30, arcade.color.GREEN)
+        arcade.draw_rectangle_filled(15, SH / 2, 30, SH - 60, arcade.color.RED)
+        arcade.draw_rectangle_filled(SW - 15, SH / 2, 30, SH - 60, arcade.color.YELLOW)
+        for box in self.boxes:
+            box.draw_box()
+    def on_update(self, dt):
+        for box in self.boxes:
+            box.update_box()
+
+def myprogram():
+    window = MyGame(SW,SH,"30_Boxes")
+    arcade.run()
+
+if __name__ == "__main__":
+    myprogram()
