03a3a8aaa10dafcf93131158cbaa26edfebf4866
hubbyada <hubbyada@urbandaleschools.com>
2022-02-23 12:17:15 -0600

    aaaaaa

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -1,28 +1,28 @@
 '''
-Sign your name:________________
+Sign your name:Adan Hubby
  
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+320,240
 2.) What are the variables dx and dy?
-
+dx and dy are velocities, changing the variable changes the direction it moves and speed
 3.) How many pixels/sec does the ball move in the x-direction?
-
+3
 4.) How many pixels/sec does the ball move in the y-direction?
-
+2
 5.) Which method is run 60 times/second?
-
+on_update
 6.) What does this code do?   self.dx *= -1
-
+It makes x move backwards or in the opposite direction
 7.) What does this code do?  self.pos_y += self.dy
-
+changes y
 8.) What is the width of the window?
-
+640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+if the ball is hitting the ceiling
 10.) What is this code checking? if self.pos_x < self.rad
-
+if the ball is hitting the left side of the screen
 
 
 '''
