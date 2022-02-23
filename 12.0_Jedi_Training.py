e23605647ae54d1ba105ec9e3934c90f04234e21
Joseph Jarman <57240500+Jarmander333@users.noreply.github.com>
2022-02-23 12:15:55 -0600

    Update 12.0_Jedi_Training.py
    Whoops!

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -4,25 +4,25 @@ Sign your name:________________
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
-
+SW/2, SH/2 (Middle of screen)
 2.) What are the variables dx and dy?
-
+Velocity
 3.) How many pixels/sec does the ball move in the x-direction?
-
+180p/s
 4.) How many pixels/sec does the ball move in the y-direction?
-
+120p/s
 5.) Which method is run 60 times/second?
-
+onUpdate()
 6.) What does this code do?   self.dx *= -1
-
+flips the horizontal axis of the ballw
 7.) What does this code do?  self.pos_y += self.dy
-
+takes the ball's old position, and adds the momentum to it.
 8.) What is the width of the window?
-
+640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+If the ball is too far down it won't go down further
 10.) What is this code checking? if self.pos_x < self.rad
-
+If the ball is too far to the left, it won't go further
 
 
 '''
