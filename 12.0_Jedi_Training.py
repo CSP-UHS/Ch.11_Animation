5bf210cc70535feb767bf9212b2fa73a5cb73e22
swartzaid <swartzaid@urbandaleschools.com>
2022-02-23 12:06:19 -0600

    Jedi training commit.

--- 12.0_Jedi_Training.py
+++ 12.0_Jedi_Training.py
@@ -3,26 +3,26 @@ Sign your name:________________
  
 All questions are about the final code in Chapter 12:
 
-1.) Where is the ball's original location?
-
-2.) What are the variables dx and dy?
-
+1.) Where is the ball's original location? The middle of the screen. The very middle
+(320,240)
+2.) What are the variables dx and dy? The direction and speed that the ball is moving.
+delta x and delta y
 3.) How many pixels/sec does the ball move in the x-direction?
-
+1
 4.) How many pixels/sec does the ball move in the y-direction?
-
+1
 5.) Which method is run 60 times/second?
-
+on_update
 6.) What does this code do?   self.dx *= -1
-
+Bounces the ball of of the wall.
 7.) What does this code do?  self.pos_y += self.dy
-
+moves the ball up equal to delta y
 8.) What is the width of the window?
-
+640
 9.) What is this code checking?  self.pos_y > SH - self.rad:
-
+If the ball has hit the ceiling.
 10.) What is this code checking? if self.pos_x < self.rad
-
+If the ball hit the left side.
 
 
 '''
