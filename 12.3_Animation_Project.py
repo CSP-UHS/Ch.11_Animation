cfee7679647cc70c82f583de1b8433b4484f6618
bakerkai <bakerkai@urbandaleschools.com>
2022-03-02 11:44:58 -0600

    *more excuses incoming for not completing animation project*

--- 12.3_Animation_Project.py
+++ 12.3_Animation_Project.py
@@ -5,6 +5,7 @@ import random
 SW = 600
 SH = 600
 FN = 300
+BW = 15
 
 class Flake:
     def __init__(self, x, y, dy, r, c):
@@ -13,33 +14,25 @@ class Flake:
         self.dy = dy
         self.rad = r
         self.col = c
-        # for second object
-        self.x2 = x
-        self.y2 = y
-        self.dy2 = dy
-        self.rad2 = r
-        self.col2 = c
+
 
     def draw_flake(self):
-        arcade.draw_circle_filled(self.x, self.y, self.rad, self.col)
-        arcade.draw_circle_filled(self.x2, self.y2, 15, arcade.color.GOLD)
+        arcade.draw_rectangle_filled(55, 55, 80, 40, arcade.color.DARK_GREEN)
+        #arcade.draw_rectangle_filled(55, 55, 10, 10, arcade.color.DARK_GREEN)
+        arcade.draw_rectangle_filled(self.x, self.y, self.rad, 5, self.col)
 
+        arcade.draw_triangle_filled(self.x, self.y, 20, 30, 40, 50, arcade.color.RED)
     def update_flake(self):
         self.y += self.dy
-        if self.y <= -self.rad:
-            self.y = random.randint(SH, SH+100)
-            self.x = random.randint(0, SW)
-    def update_enm(self):
-        self.y2 += self.dy
-        if self.y2 <= -self.rad:
-            self.y2 = random.randint(SH, SH+100)
-            self.x2 = random.randint(0, SW)
+        if self.x <= -self.rad:
+            self.x = random.randint(SW, SW+100)
+            self.y = random.randint(0, SW)
+
 class MyGame(arcade.Window):
     def __init__(self, SW, SH, title):
         super().__init__(SW, SH, title)
         arcade.set_background_color(arcade.color.SKY_BLUE)
         self.flakelist = []    #  hold all snow flake
-        #   create flake
         for i in range(FN):
             r = random.randint(10, 20)
             dy = random.randint(-4, -1)
@@ -57,10 +50,11 @@ class MyGame(arcade.Window):
         arcade.start_render()
         for flake in self.flakelist:
             flake.draw_flake()
-        arcade.draw_rectangle_filled(SW//2, SH//2, 10, SH, arcade.color.WHITE_SMOKE)
-        arcade.draw_rectangle_filled(SW//2, 20, SW, 70, arcade.color.DARK_BROWN)
-        arcade.draw_rectangle_filled(SW//2, 600, SW, 70, arcade.color.DARK_BROWN) #reverse this
-        arcade.draw_rectangle_filled(SW//2, SH//2, SW, 10, arcade.color.WHITE_SMOKE)
+        arcade.draw_rectangle_filled(SW, SH//2, 20, SH, arcade.color.GREEN)
+        arcade.draw_rectangle_filled(BW//2, SH//2, BW, SH, arcade.color.GREEN)
+        arcade.draw_rectangle_filled(SW//2, 3, SW, 25, arcade.color.GREEN)
+        arcade.draw_rectangle_filled(SW//2, 600, SW, 25, arcade.color.GREEN) #reverse this
+        #arcade.draw_rectangle_filled(SW//2, SH//2, SW, 10, arcade.color.WHITE_SMOKE)
 
     def on_update(self, dt):
         for flake in self.flakelist:
