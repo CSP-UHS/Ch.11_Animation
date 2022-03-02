1b2fc427365d5cd8f673a982db3388bfc53dbbe8
clarksongav <ClarksonGav@urbandaleschools.com>
2022-03-02 12:16:52 -0600

    spopers

--- 12.3_Animation_Project.py
+++ 12.3_Animation_Project.py
@@ -6,43 +6,46 @@ Your choice!!! Have fun and be creative.
 '''
 import arcade
 import random
+import turtle
 SW = 600
 SH = 600
 WY = -1
 WX = -1
+BC = 0
+totalBC = 0
 class Ball():
-    def __init__(self,pos_x,pos_y,dx,dy,rad,col):
+    def __init__(self,pos_x,pos_y,dx,dy,rad,col,BC):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.dx = dx
         self.dy = dy
         self.rad = rad
         self.col = col
-
+        self.BC = BC
 
     def draw_ball(self):
-        arcade.draw_circle_outline(self.pos_x, self.pos_y, self.rad, arcade.color.BLACK, rad * 2.05)
+        arcade.draw_circle_outline(self.pos_x, self.pos_y, self.rad, arcade.color.BLACK, self.rad * 2.05)
         arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad, arcade.color.WHITE)
-        arcade.draw_circle_outline(self.pos_x, self.pos_y, self.rad / 1.5, self.col, rad)
+        arcade.draw_circle_outline(self.pos_x, self.pos_y, self.rad / 1.5, self.col, self.rad)
         arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad / 1.6, self.col)
         arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad / 3, arcade.color.BLACK)
         arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad / 3.4, arcade.color.LIGHT_STEEL_BLUE)
-        arcade.draw_arc_filled(self.pos_x, self.pos_y + self.rad / 1.15, rad * 1.2, rad * 1.25, arcade.color.BLACK, 0, 180, )
-        arcade.draw_arc_filled(self.pos_x, self.pos_y + self.rad / 1.1, rad * 1.1, rad * 1.1, arcade.color.WHITE, 0, 180)
-        arcade.draw_circle_filled(self.pos_x, self.pos_y + rad * 1.15, rad / 5, arcade.color.BLACK)
-        arcade.draw_circle_filled(self.pos_x, self.pos_y + rad * 1.15, rad / 5.5, self.col)
+        arcade.draw_arc_filled(self.pos_x, self.pos_y + self.rad / 1.15, self.rad * 1.2, self.rad * 1.25, arcade.color.BLACK, 0, 180, )
+        arcade.draw_arc_filled(self.pos_x, self.pos_y + self.rad / 1.1, self.rad * 1.1, self.rad * 1.1, arcade.color.WHITE, 0, 180)
+        arcade.draw_circle_filled(self.pos_x, self.pos_y + self.rad * 1.15, self.rad / 5, arcade.color.BLACK)
+        arcade.draw_circle_filled(self.pos_x, self.pos_y + self.rad * 1.15, self.rad / 5.5, self.col)
 
     def update_ball(self):
         self.pos_x += self.dx
         self.pos_y += self.dy
 
         #bounce ball off of walls
-        if self.pos_x > SW-self.rad*3 or self.pos_x < self.rad*3:
+        if self.pos_x > SW-self.rad or self.pos_x < self.rad:
             self.dx*=-1
-
-        if self.pos_y > SH - self.rad*3 or self.pos_y < self.rad*3:
+            self.BC += 1
+        if self.pos_y > SH - self.rad*1.5 or self.pos_y < self.rad:
             self.dy *= -1
-
+            self.BC += 1
 
 
 
@@ -52,18 +55,26 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.WHITE)
         self.balls =[]
         for i in range(10):
-            r = random.randint(2,20)
+            r = random.randint(20,50)
             x = random.randint(r,SW-r)
-            y = random.randint(r,SH-r)
+            y = random.randint(r,SH-r*2)
             dx = random.randint(-3,3)
+            if dx == 0:
+                dx = 1
             dy = random.randint(-3,3)
+            if dy == 0:
+                dy = -1
             c = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-            ball = Ball(x,y,dx,dy,r,c)
+
+            ball = Ball(x,y,dx,dy,r,c,0)
             self.balls.append(ball)
     def on_draw(self):
         arcade.start_render()
+
         for ball in self.balls:
-            ball.draw_ball(300,300)
+            ball.draw_ball()
+
+            arcade.draw_text(str(ball.BC),ball.pos_x-ball.rad/6,ball.pos_y-ball.rad/6,arcade.color.BLACK,20,50)
 
     def on_update(self, dt):
         for ball in self.balls:
