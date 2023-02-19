08b6b4a84cfc4dec0924a4a49c85dc3ffa418283
huynhlin <huynhlin@urbandaleschools.com>
2023-02-18 18:30:15 -0600

    ch 11

--- 11.1_Thirty_Box_Bounce.py
+++ 11.1_Thirty_Box_Bounce.py
@@ -22,3 +22,146 @@ Helpful Hints:
 4.) Also in the on_draw section draw the side rails.
 5.) In the on_update section use: for box in self.boxlist: box.update_box()
 '''
+import arcade
+import random
+
+SW = 600
+SH = 600
+
+pallete = ["dd4444", "f48080", "2d676f", "194b4f", "ffdcdc"]
+
+
+
+# class Arrow:
+#     def __init__(self, fx, fy, sy, sx, tx, ty, cor):
+#         self.fx = fx
+#         self.sx = sx
+#         self.tx = tx
+#
+#         self.fy = fy
+#         self.sy = sy
+#         self.ty = ty
+#         self.cor = cor
+#
+#     def draw_arrow(self):
+#         arcade.draw_triangle_filled(self.fx, self.fy + 300, self.sx, self.sy + 300, self.tx, self.ty + 300, self.cor)
+#         arcade.draw_text("PAUSED", SW/2, SH*7/8, arcade.color.WHITE, font_size=25, anchor_x="center")
+#
+#     def update_arrow(self, dt, velocity):
+#         while self.sy > -100:
+#             self.fy -= velocity*dt
+#             self.sy -= velocity*dt
+#             self.ty -= velocity*dt
+#
+#     def reset_arrow(self, dt, velocity):
+#         while self.sy < -100:
+#             self.fy += velocity*dt
+#             self.sy += velocity*dt
+#             self.ty += velocity*dt
+class Box:
+    def __init__(self, pos_x, pos_y, dx, dy, w, h, color):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.dx = dx
+        self.dy = dy
+        self.w = w
+        self.h = h
+        self.color = color
+
+    def draw_box(self):
+        arcade.draw_rectangle_filled(self.pos_x, self.pos_y, self.w, self.h, self.color)
+
+    def update_box(self, dt):
+        # bounces box off edge of bars and changes color
+        if self.pos_x + round(self.w/2) + 30 >= SW:
+            self.dx *= -1
+            self.color = arcade.color_from_hex_string(pallete[3])
+        if self.pos_x < round(self.w/2) + 30:
+            self.dx *= -1
+            self.color = arcade.color_from_hex_string(pallete[2])
+
+        if self.pos_y + round(self.h/2) >= SH - 30:
+            self.dy *= -1
+            self.color = arcade.color_from_hex_string(pallete[0])
+        if self.pos_y - round(self.h/2) <= 0 + 30:
+            self.dy *= -1
+            self.color = arcade.color_from_hex_string(pallete[1])
+
+        self.pos_x += self.dx * dt
+        self.pos_y += self.dy * dt
+
+
+class MyGame(arcade.Window):
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.WHITE)
+
+        self.timeScale = 1
+        self.timeScaleD = 0.0275
+
+        self.boxes = []
+        for i in range(30):
+            w = random.randint(10, 50)
+            h = w
+            x = random.randint(w+30, SW-w-30)
+            y = random.randint(w+30, SH-w-30)
+            vx = random.randint(-5, 5)
+            while vx == 0:
+                vx = random.randint(-5, 5)
+            vy = random.randint(-5, 5)
+            while vy == 0:
+                vy = random.randint(-5, 5)
+            c = arcade.color.BLACK
+            self.box = Box(x, y, vx, vy, w, h, c)
+            self.boxes.append(self.box)
+
+        # self.arrows = []
+        # fx = SW / 3
+        # sx = SW / 3
+        # tx = SW * 2/3
+        #
+        # fy = SH * 2 / 3
+        # sy = SH / 3
+        # ty = SH / 2
+        # cor = arcade.color.WHITE
+        # self.arrow = Arrow(fx, fy, sx, sy, tx, ty, cor)
+        # self.arrows.append(self.arrow)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.set_background_color(arcade.color_from_hex_string(pallete[4]))
+
+        arcade.draw_rectangle_filled(SW / 2, SH - 15, SW, 30, arcade.color_from_hex_string(pallete[0]))
+        arcade.draw_rectangle_filled(SW / 2, 15, SW, 30, arcade.color_from_hex_string(pallete[1]))
+
+        arcade.draw_rectangle_filled(15, SH / 2, 30, SH, arcade.color_from_hex_string(pallete[2]))
+        arcade.draw_rectangle_filled(SW - 15, SH / 2, 30, SH, arcade.color_from_hex_string(pallete[3]))
+        for box in self.boxes:
+            box.draw_box()
+        # for arrow in self.arrows:
+        #     arrow.draw_arrow()
+
+    def on_update(self, dt):
+        self.timeScale = max(min(self.timeScale + self.timeScaleD, 1), 0)
+        for box in self.boxes:
+            box.update_box(self.timeScale)
+
+    def on_key_press(self, key, mod):
+        if key == arcade.key.SPACE:
+            self.timeScaleD *= -1
+            # v = 10
+            # for arrow in self.arrows:
+            #     if arrow.sy > 100:
+            #         arrow.update_arrow(self.timeScale, v)
+            #     else:
+            #         arrow.reset_arrow(self.timescale, v)
+
+
+def main():
+    MyGame(SW, SH, "30 Boxes")
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
--- 11.2_Snowfall.py
+++ 11.2_Snowfall.py
@@ -4,16 +4,101 @@ SNOWFALL
 Try to create the snowfall animation by meeting
 the following requirements:
 
-1.) Create a 600 x 600 window with black background
-2.) Window title equals "Snowfall"
-3.) Crossbars 10 px wide. Snow must be outside!
-4.) Make snowflake radius random between 1-3
-5.) Randomly start snowflakes anywhere in the window.
-6.) Random downward speed of -4 to -1
-7.) Start snowflakes again at random x from 0-600 and random y from 600-700
-8.) Generate 300 snowflakes
-9.) Color snowflake #1 red just for fun.
-10.) All other snowflakes should be white.
+(1.) Create a 600 x 600 window with black background
+(2.) Window title equals "Snowfall"
+(3.) Crossbars 10 px wide. Snow must be outside!
+(4.) Make snowflake radius random between 1-3
+(5.) Randomly start snowflakes anywhere in the window.
+(6.) Random downward speed of -4 to -1
+(7.) Start snowflakes again at random x from 0-600 and random y from 600-700
+(8.) Generate 300 snowflakes
+(9.) Color snowflake #1 red just for fun.
+(10.) All other snowflakes should be white.
 
 
 '''
+
+import arcade
+import random
+
+SW = 600
+SH = 600
+done = False
+
+
+class Snow:
+    def __init__(self, pos_x, pos_y, dy, rad, color):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.dy = dy
+        self.rad = rad
+        self.color = color
+
+    def draw_ball(self):
+        arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad, self.color)
+
+    def update_ball(self):
+
+        # if self.pos_y + self.rad >= SH or self.pos_y - self.rad <= 0:
+        #     self.dy *= -1
+
+        self.pos_y += self.dy
+
+        if self.pos_y <= -self.rad:
+            self.pos_x = random.randint(0, SW)
+            self.pos_y = random.randint(SH, SH + 100)
+            self.dy = random.randint(-5, -2)
+
+        if round(SW / 2) - 5 - self.rad < self.pos_x < round(SW / 2) + 5 + self.rad:
+            self.pos_x += 10
+        if round(SW / 2) - 5 - self.rad < self.pos_y < round(SW / 2) + 5 + self.rad:
+            self.pos_y -= 10
+
+
+class Snowfall(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.BLACK)
+        self.snowflakes = []
+
+        for i in range(0, 350):
+            r = random.randint(1, 3)
+            x = random.randint(r, SW - r)
+            while round(SW/2) - 5 < x < round(SW/2) + 5:
+                x = random.randint(r, SW - r)
+            y = random.randint(600, 790)
+            while round(SW / 2) - 5 < y < round(SW / 2) + 5:
+                y = random.randint(r, SH - r)
+            vy = random.randint(-5, -2)
+            while vy == 0:
+                vy = random.randint(-6, -2)
+            if i == 1:
+                c = arcade.color.RED
+            else:
+                c = arcade.color.WHITE
+            self.snowflake = Snow(x, y, vy, r, c)
+            self.snowflakes.append(self.snowflake)
+
+    def on_draw(self):
+        arcade.start_render()
+
+        # window
+        arcade.draw_rectangle_filled(round(SW / 2), round(SH / 2), 10, SH, arcade.color.COFFEE)
+        arcade.draw_rectangle_filled(round(SW / 2), round(SH / 2), SW, 10, arcade.color.COFFEE)
+
+        for snowflake in self.snowflakes:
+            snowflake.draw_ball()
+
+    def on_update(self, dt):
+        for snowflake in self.snowflakes:
+            snowflake.update_ball()
+
+
+def main():
+    Snowfall(SW, SH, "Snowfall")
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
+    
