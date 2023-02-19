ce23a311fc480f0f0be556754c05507d652b608f
huynhlin <huynhlin@urbandaleschools.com>
2023-02-18 18:33:13 -0600

    ch 11

--- 11.0_Jedi_Training.py
+++ 11.0_Jedi_Training.py
@@ -1,11 +1,15 @@
 '''
-Sign your name:________________
+Sign your name:Lindy
  
 All questions are about the final code in Chapter 12:
+
+# yo i dont have the code that we're supposed to do this assignment on but i'll answer the ones that i can
 
 1.) Where is the ball's original location?
 
 2.) What are the variables dx and dy?
+
+delta x and y, i.e the change in x and y
 
 3.) How many pixels/sec does the ball move in the x-direction?
 
@@ -13,15 +17,27 @@ All questions are about the final code in Chapter 12:
 
 5.) Which method is run 60 times/second?
 
+update
+
 6.) What does this code do?   self.dx *= -1
+
+makes the delta x negative, reversing the velocity in effect
 
 7.) What does this code do?  self.pos_y += self.dy
 
+updates the ball, the y position is increased by the change in y
+
 8.) What is the width of the window?
+
+SW variable's value
 
 9.) What is this code checking?  self.pos_y > SH - self.rad:
 
+if the ball is touching the top of the screen
+
 10.) What is this code checking? if self.pos_x < self.rad
+
+if the ball is touching the left side of the screen
 
 
 
