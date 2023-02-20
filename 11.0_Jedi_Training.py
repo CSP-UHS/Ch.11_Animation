7fce56a60de922bd46c10ac515d8644d4f469c23
haraziode <haraziode@urbandaleschools.com>
2023-02-20 13:48:34 -0600

    Completed Chapter 11 - Oded

--- 11.0_Jedi_Training.py
+++ 11.0_Jedi_Training.py
@@ -1,27 +1,38 @@
 '''
-Sign your name:________________
+Sign your name: Oded Harazi
  
 All questions are about the final code in Chapter 12:
 
 1.) Where is the ball's original location?
+Anywhere inside the window where the ball(s) can fully be seen.
 
 2.) What are the variables dx and dy?
+---The derivatives of x and y. Determine the velocity of the ball and in what direction.
 
 3.) How many pixels/sec does the ball move in the x-direction?
+---dx * 60
 
 4.) How many pixels/sec does the ball move in the y-direction?
+---dy * 60
 
 5.) Which method is run 60 times/second?
+--- "Update" Method (dx and dy)
 
 6.) What does this code do?   self.dx *= -1
+---Reverses the direction of the ball.
 
 7.) What does this code do?  self.pos_y += self.dy
+---Moves the ball along the y-axis dy pixels, from where it was 60 times per second.
 
 8.) What is the width of the window?
+---SW
 
 9.) What is this code checking?  self.pos_y > SH - self.rad:
+---Making sure that the ball doesn't float off the top or bottom edge of the window.
 
 10.) What is this code checking? if self.pos_x < self.rad
+---Making sure that the ball doesn't float off the right or left edge of the window.
+
 
 
 
--- 11.2_Snowfall.py
+++ 11.2_Snowfall.py
@@ -17,3 +17,66 @@ the following requirements:
 
 
 '''
+import arcade
+import random
+SW = 600
+SH = 600
+
+
+class Snowflake():
+    def __init__(self,pos_x,pos_y,dx,dy,rad,col):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.dx = dx
+        self.dy = dy
+        self.rad = rad
+        self.col = col
+    def draw_snow(self):
+        arcade.draw_circle_filled(self.pos_x, self.pos_y ,self.rad,self.col)
+
+    def draw_window(self):
+        arcade.draw_rectangle_filled(300,300,600,10,arcade.color.REDWOOD)
+        arcade.draw_rectangle_filled(300,300,600,10,arcade.color.REDWOOD,90)
+
+    def update_snow(self):
+        self.pos_y+=self.dy
+        if self.pos_y<=0:
+             self.pos_y = random.randint(600,700)
+             self.pos_x = random.randint(0, 600)
+
+
+class MyGame(arcade.Window):
+    def __init__(self,width,height,title):
+        super().__init__(width,height,title)
+        arcade.set_background_color(arcade.color.BLACK)
+        self.snowflakes_list=[]
+        for i in range(300):
+            r=random.randint(1,3)
+            x=random.randint(1,600)
+            y=random.randint(0,600)
+            vx=random.randint(-4,-1)
+            vy=random.randint(-4,-1)
+            c = arcade.color.WHITE
+            w = arcade.color.RED
+            self.snowflake = Snowflake(x,y,vx,vy,r,c)
+            self.snowflakes_list.append(self.snowflake)
+        self.redflake = Snowflake(x, y, vx, vy, 3, w)
+        self.snowflakes_list.append(self.redflake)
+
+    def on_draw(self):
+        arcade.start_render()
+        for item in (self.snowflakes_list):
+            item.draw_snow()
+        arcade.draw_rectangle_filled(300, 300, 600, 10, arcade.color.REDWOOD)
+        arcade.draw_rectangle_filled(300, 300, 600, 10, arcade.color.REDWOOD, 90)
+
+
+    def on_update(self,dt):
+        for item in (self.snowflakes_list):
+            item.update_snow()
+
+def main():
+    window = MyGame(SW,SH,"Snowfall")
+    arcade.run()
+if __name__ == "__main__":
+    main()
